26f9848431ea8ce4ff649ed1e22ef0624e3feba9
Joe Ovett <j.ovett1@uni.brighton.ac.uk>
2024-04-27 17:27:22 +0100

    login php/sql working
    first successful brighton.domains sql record

---
+++ server/database.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = "localhost";
+$dbname = "jo435_login_db";
+$username = "jo435_ica";
+$password = "!nfection123";
+
+$mysqli = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
+                     
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
+}
+
+return $mysqli; 
+
+
